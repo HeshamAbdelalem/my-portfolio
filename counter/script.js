4f01cf6038b960a2a1bdad630ac7e6c4af6d9c05
HeshamAbdelalem <hesham.abdelalem1@gmail.com>
2020-09-26 01:41:26 +0200

    counter: adds vibrate when click

--- counter/script.js
+++ counter/script.js
@@ -8,17 +8,17 @@ countNum = 0;
 increaseBtn.addEventListener('click', () => {
   countNum = countNum + 1;
   count.textContent = countNum;
-  navigator.vibrate(100);
+  navigator.vibrate(50);
 });
 
 decreaseBtn.addEventListener('click', () => {
   countNum = countNum - 1;
   count.textContent = countNum;
-  navigator.vibrate(100);
+  navigator.vibrate(50);
 });
 
 resetBtn.addEventListener('click', () => {
   countNum = 0;
   count.textContent = countNum;
-  navigator.vibrate(100);
+  navigator.vibrate(300);
 });
